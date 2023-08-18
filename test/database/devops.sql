9389d78a86e3c18f9eec3fb78f5ba2dc2342f355
JoseMarulandaLuguu <josemarulanda.luguu@gmail.com>
2023-08-17 19:24:57 -0500

    database sql file and test data updated

--- test/database/devops.sql
+++ test/database/devops.sql
@@ -23,13 +23,14 @@ SET row_security = off;
 -- Dependencies: 212
 -- Data for Name: Inventions; Type: TABLE DATA; Schema: public; Owner: -
 --
-
+DROP TABLE IF EXISTS public."Inventions";
+DROP TABLE IF EXISTS public."Inventors";
 --
 -- TOC entry 4279 (class 0 OID 17189)
 -- Dependencies: 210
 -- Data for Name: Inventors; Type: TABLE DATA; Schema: public; Owner: -
 --
-CREATE TABLE /*IF NOT EXISTS*/ public."Inventors"
+CREATE TABLE IF NOT EXISTS public."Inventors"
 (
     id SERIAL NOT NULL ,
     name character varying COLLATE pg_catalog."default",
@@ -40,7 +41,7 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public."Inventors"
     OWNER to devops;
 
-CREATE TABLE /*IF NOT EXISTS*/ public."Inventions"
+CREATE TABLE IF NOT EXISTS public."Inventions"
 (
     id SERIAL NOT NULL ,
     name character varying COLLATE pg_catalog."default" NOT NULL,
@@ -59,17 +60,17 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public."Inventions"
     OWNER to devops;
 
-INSERT INTO public."Inventors" (id, name) VALUES (2, 'Thomas Edison');
-INSERT INTO public."Inventors" (id, name) VALUES (3, 'Nikola Tesla');
-INSERT INTO public."Inventors" (id, name) VALUES (4, 'Marie Curie');
-
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (1 'Incandescent Light Bulb', '1879', ' Edison''s light bulb revolutionized indoor lighting by providing a practical and efficient source of electric light', 2);
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (2, 'Phonograph', '1877', 'The phonograph was a device that could record and reproduce sound, pioneering the audio recording industry', 2);
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (5, 'Alternating Current (AC) System', '1880s', 'Tesla''s AC system transformed the way electricity is transmitted and distributed, making long-distance power transmission possible', 3);
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (6, 'Tesla Coil', '1891', 'The Tesla Coil is an electrical resonant transformer circuit used for generating high-voltage, low-current, high-frequency alternating-current electricity', 3);
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (7, 'X-ray Machine', '1896', 'Marie Curie''s work in X-rays included the development of portable X-ray machines for use in field hospitals during World War I', 4);
-INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (8, 'Radium and Polonium Isolation', '1898', 'Curie isolated and identified the radioactive elements radium and polonium, contributing to the understanding of radioactivity', 4);
-
+INSERT INTO public."Inventors" (name) VALUES ('Thomas Edison');
+INSERT INTO public."Inventors" (name) VALUES ('Nikola Tesla');
+INSERT INTO public."Inventors" (name) VALUES ('Marie Curie');
+/*
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (1 'Incandescent Light Bulb', "1879", "Edison''s light bulb revolutionized indoor lighting by providing a practical and efficient source of electric light", 1);
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (2, 'Phonograph', '1877', 'The phonograph was a device that could record and reproduce sound, pioneering the audio recording industry', 1);
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (3, 'Alternating Current (AC) System', '1880s', 'Tesla''s AC system transformed the way electricity is transmitted and distributed, making long-distance power transmission possible', 2);
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (4, 'Tesla Coil', '1891', 'The Tesla Coil is an electrical resonant transformer circuit used for generating high-voltage, low-current, high-frequency alternating-current electricity', 2);
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (5, 'X-ray Machine', '1896', 'Marie Curie''s work in X-rays included the development of portable X-ray machines for use in field hospitals during World War I', 3);
+INSERT INTO public."Inventions" (id, name, year, description, inventor) VALUES (6, 'Radium and Polonium Isolation', '1898', 'Curie isolated and identified the radioactive elements radium and polonium, contributing to the understanding of radioactivity', 3);
+*/
 
 --
 -- TOC entry 4290 (class 0 OID 0)
--- test/inventors/inventors.data.js
+++ test/inventors/inventors.data.js
@@ -1,12 +1,12 @@
 const getInventors = [
-    { "id": 2, "name": "Thomas Edison"},
-    { "id": 3, "name": "Nikola Tesla" },
-    { "id": 4, "name": "Marie Curie"}
+    { "id": 1, "name": "Thomas Edison"},
+    { "id": 2, "name": "Nikola Tesla" },
+    { "id": 3, "name": "Marie Curie"}
 ]
 
 const postInventor = {name:"Copernico"}
-const putInventor = {id:5,name:"Don copernico"}
-const deleteInventor = 5;
+const putInventor = {id:4,name:"Don copernico"}
+const deleteInventor = 4;
 
 module.exports = {
     getInventors,
